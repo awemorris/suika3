bef582652d483f9e1479218b8877b813d34c65a7
Awe Morris <awe@noctvm.io>
2026-04-06 00:31:17 +0900

    Fix NetBSD build

--- external/PlayfieldEngine/external/StratoHAL/cmake/modules/FindPNG.cmake
+++ external/PlayfieldEngine/external/StratoHAL/cmake/modules/FindPNG.cmake
@@ -1,6 +1,6 @@
 find_path(PNG_INCLUDE_DIR png.h)
 
-find_library(PNG_LIBRARY NAMES png png_static libpng libpng_static)
+find_library(PNG_LIBRARY NAMES png png_static png16 libpng libpng_static libpng16)
 
 include(FindPackageHandleStandardArgs)
 find_package_handle_standard_args(PNG DEFAULT_MSG PNG_LIBRARY PNG_INCLUDE_DIR)
