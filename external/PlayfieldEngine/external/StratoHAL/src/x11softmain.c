91b038d83aa48cd2c55dd4676bba4d7307a506fc
Awe Morris <awe@noctvm.io>
2026-06-28 00:06:20 +0900

    [fix] Fix for SPARC64

--- external/PlayfieldEngine/external/StratoHAL/src/x11softmain.c
+++ external/PlayfieldEngine/external/StratoHAL/src/x11softmain.c
@@ -901,7 +901,7 @@ run_frame(void)
 					uint32_t g = (pix >> 8) & 0xff;
 					uint32_t b = (pix >> 16) & 0xff;
 					uint32_t a = (pix >> 24) & 0xff;
-					*src++ = a | (b << 8) | (g << 16) | (r << 24);
+					*src++ = a | (r << 8) | (g << 16) | (b << 24);
 				}
 			}
 		} else
